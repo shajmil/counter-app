5fdfaa01c250f394cc0230564c91509616de4c76
Shajmil VJ <v.jshejmil@gmail.com>
2022-12-07 12:44:24 +0530

    React_counter-app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
+import { useState} from 'react'
 import './App.css';
-
+import { Button } from 'reactstrap';
 function App() {
+const [counter, setstate] = useState(0)
+var increment=() => {
+  setstate(counter+1)
+}
+var decrement=() => {
+  if(counter>0){
+
+    setstate(counter-1)
+  }
+}
+var reset=() => {
+
+  setstate(0)
+}
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='border border-3 p-5'>
+    <h1> Counter App </h1>
+ 
+    <h2> {counter}</h2>
+    <div className="d-flex gap-3" >
+   
+    <button onClick={increment} className="btn btn-primary" >Increase</button>
+    <button onClick={decrement} className="btn btn-danger" >Decrease</button>
+    <button onClick={reset} className="btn btn-secondary" >Reset</button>
+          </div> 
+    </div>
       </header>
     </div>
   );
